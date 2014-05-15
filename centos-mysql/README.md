eda96a97e2be6e6e29901a76d7e46c4a554d6a37
Dane Lipscombe <dane@lipscombe.com.au>
2014-05-15 15:06:55 +0900

    Improve mysql

--- centos-mysql/README.md
+++ centos-mysql/README.md
@@ -1,1 +1,25 @@
-Work in progress..
+## MySQL Dockerfile
+
+### Basic Usage
+
+Default settings
+
+Database: data  
+User: admin  
+Password: adminpass  
+
+```sh
+docker run -d dlip/centos-mysql
+```
+
+### Import sql file on start
+
+```sh
+docker run -d -v <path_to_data>:/data -e "MYSQL_IMPORT_FILE=/data/dump.sql" dlip/centos-mysql
+```
+
+### Use different credentials/database name
+
+```sh
+docker run -d -e "MYSQL_DATABASE=mydatabase" -e "MYSQL_USER=myuser" -e "MYSQL_PASSWORD=mypass" dlip/centos-mysql
+```
--- centos-mysql/start.sh
+++ centos-mysql/start.sh
@@ -3,8 +3,16 @@ chown mysql:mysql -R /var/lib/mysql
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/my.cnf
 
 service mysqld start
-echo "GRANT ALL ON *.* TO $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
-echo "GRANT ALL ON *.* TO $MYSQL_USER@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
-echo "CREATE DATABASE $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci" | mysql
+
+if [[ ! -d /var/lib/mysql/$MYSQL_DATABASE ]] ; then
+  echo "GRANT ALL ON *.* TO $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+  echo "GRANT ALL ON *.* TO $MYSQL_USER@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+  echo "CREATE DATABASE $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci" | mysql
+
+  if [ -n "$MYSQL_IMPORT_FILE" ]; then
+    echo "Importing $MYSQL_IMPORT_FILE"
+    mysql $MYSQL_DATABASE < $MYSQL_IMPORT_FILE
+  fi
+fi
 
 tail -f /var/log/mysqld.log
