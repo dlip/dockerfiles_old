2286651fbcfb63dde19a8e3a43b53f9745d1107a
Dane Lipscombe <dane@lipscombe.com.au>
2014-05-17 23:36:17 +0900

    Changing templating

--- centos-nginx/start.sh
+++ centos-nginx/start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-jinja2 /etc/nginx/nginx.conf.jinja2 > /etc/nginx/nginx.conf
-
-for f in $(bash -c "cd /nginx-templates/; find * 2> /dev/null"); do
-  jinja2 "$f" > /etc/nginx/conf.d/$f
-done
+bash -c "minga /etc/minga/nginx /etc/minga/nginx /etc/nginx '$NGINX_DEFAULT_OPT' '$NGINX_OPT'"
 
 exec nginx
